293787cd15c1d3422889f0b1af996f746afb26f3
Brayan-Sutil <brayansutil81@gmail.com>
2023-11-12 19:07:15 -0300

    feat: change function open cart

--- src/app/components/AppBarComponent/AppBarComponent.tsx
+++ src/app/components/AppBarComponent/AppBarComponent.tsx
@@ -5,8 +5,11 @@ import Button from "@mui/material/Button";
 import LocalGroceryStoreIcon from "@mui/icons-material/LocalGroceryStore";
 import Logo from "./Logo/Logo";
 import { CustomToolbar } from "./styled";
+import { useMks } from "@/app/Context/MksContext";
 
 const AppBarComponent = () => {
+  const { toggleDrawer } = useMks()
+  
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" component="div" sx={{ bgcolor: "#0F52BA" }}>
@@ -17,6 +20,7 @@ const AppBarComponent = () => {
             startIcon={<LocalGroceryStoreIcon />}
             color="primary"
             sx={{ color: "black", bgcolor: "white" }}
+            onClick={() => toggleDrawer(true)}
           >
             5
           </Button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Wapper from "./components/Wapper/Wapper";
 import "../app/global.css";
+import { MksProvider } from "./Context/MksProvider";
 
 export default function Home() {
   return (
-    <>
+    <MksProvider>
       <Wapper />
-    </>
+    </MksProvider>
   );
 }
