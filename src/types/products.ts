15bdac56da0fa26c70c0808c96dd13d7720830ed
Brayan-Sutil <brayansutil81@gmail.com>
2023-11-13 00:35:01 -0300

    feat: correct item box

--- src/types/products.ts
+++ src/types/products.ts
@@ -1,7 +1,8 @@
 export interface IProduct {
-    id: number
-    name: string
-    brand: string
-    description: string
-    price: number
+  id: number;
+  name: string;
+  brand: string;
+  description: string;
+  price: number;
+  photo: string;
 }
